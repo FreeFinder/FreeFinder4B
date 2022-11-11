01cf998ece55086eda6b31674dbb338dd1d80914
steven arellano <stevenjarellano2@gmail.com>
2022-11-11 08:21:06 -0600

    changed name: FreeFinders => FreeFinder

--- FreeFinder4B/ViewControllers/ViewController.swift
+++ FreeFinder4B/ViewControllers/ViewController.swift
@@ -12,16 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.insetsLayoutMarginsFromSafeArea = false
-        let initialLocation = CLLocation(latitude: 41.7886, longitude: -87.5987)
-        mapView.centerToLocation(initialLocation)
-        Task{
-            await refresh()
+        if (mapView != nil) {
+            view.insetsLayoutMarginsFromSafeArea = false
+            let initialLocation = CLLocation(latitude: 41.7886, longitude: -87.5987)
+            mapView.centerToLocation(initialLocation)
+            Task{
+                await refresh()
+            }
         }
-        // Do any additional setup after loading the view.
     }
 
-    func refresh() async -> [Item]{
+    func refresh() async -> [Item] {
         let user = await User(email: "mongodb@gmail.com");
         let observer = await AppData(user: user);
         mapView!.removeAnnotations(mapView!.annotations)
