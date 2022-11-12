1d0072f1e2381ee36c3a8a5456abb78c6abf348c
steven arellano <stevenjarellano2@gmail.com>
2022-11-12 11:25:49 -0600

    Added start screen

---
+++ FreeFinder4B/ViewControllers/StartViewController.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    @IBAction func startApp(_ sender: Any) { // this is how to do programmatic navigation
+        Task {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let TabBarController = storyBoard.instantiateViewController(withIdentifier: "TabBar") as! UITabBarController
+            self.navigationController?.pushViewController(TabBarController, animated: true);
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
