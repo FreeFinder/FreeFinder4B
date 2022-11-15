6084b9c192e4a567100d33413b35f9b9d4e5af19
Ellen Chen <ellenrchen8@gmail.com>
2022-11-15 08:28:48 -0600

    removed pfp and name

--- FreeFinder4B/Controllers/ProfileViewController.swift
+++ FreeFinder4B/Controllers/ProfileViewController.swift
@@ -3,20 +3,25 @@ import UIKit
 
 class ProfileViewController: UIViewController{
     
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var name: UILabel!
+//    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var email: UILabel!
-    @IBOutlet weak var editProfile: UIButton!
+//    @IBOutlet weak var name: UILabel!
+//    @IBOutlet weak var editProfile: UIButton!
     @IBOutlet weak var signOut: UIButton!
     
     override func viewDidLoad() {
+        print("hi");
         super.viewDidLoad()
-
-        profileImage.layer.masksToBounds = true
-        profileImage.layer.cornerRadius = profileImage.bounds.width / 2
-            
+        
+//        profileImage.layer.masksToBounds = true
+//        profileImage.layer.cornerRadius = profileImage.bounds.width / 2
+        
         // TODO: connect to data
-        name.text = "Jane Doe"
-        email.text = "test@test.com"
+//        name.text = USER!.id.stringValue
+        email.text = "\(DEVICE_DATA.email ?? "")";
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        let _ = USER!.sign_out();
     }
 }
