7fc04a715d744a23412b7486c8d21a10585ae721
steven arellano <stevenjarellano2@gmail.com>
2022-11-18 08:35:02 -0600

    decr, comments

--- FreeFinder4B/Context/AppConfig.swift
+++ FreeFinder4B/Context/AppConfig.swift
@@ -1,6 +1,7 @@
 import Foundation
 import RealmSwift
 
+// GLOBAL STATE OF OUR APP
 let DEVICE_DATA = Device();
 var USER: User? = nil;
 var APP_DATA: AppData? = nil;
--- FreeFinder4B/Context/Items.swift
+++ FreeFinder4B/Context/Items.swift
@@ -1,3 +1,5 @@
 import Foundation
 
+
 let ITEM_TAGS: Array<String> = ["Food", "Clothing", "Furniture", "Other"];
+let DECREMENT_DISTANCE = 0.01;
